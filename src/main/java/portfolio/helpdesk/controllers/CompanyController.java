d52e7e4e6970799c7a31d0c9aa531f19b4b3dddd
Daniel Gonzales <danielsmart5@hotmail.com>
2024-02-20 17:13:23 -0500

    Listado de compañías

--- src/main/java/portfolio/helpdesk/controllers/CompanyController.java
+++ src/main/java/portfolio/helpdesk/controllers/CompanyController.java
@@ -2,17 +2,16 @@ package portfolio.helpdesk.controllers;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import portfolio.helpdesk.DTO.CompanyDTO;
 import portfolio.helpdesk.mappers.CompanyMapper;
 import portfolio.helpdesk.models.Company;
 import portfolio.helpdesk.services.ICompanyService;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,5 +26,11 @@ public class CompanyController {
         Company company = companyService.save(companyMapper.convertToEntity(companyDTO));
         URI location = URI.create(String.format("/company/%d", company.getIdCompany()));
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CompanyDTO>> findAll(){
+        List<CompanyDTO> list = companyService.findAll().stream().map(companyMapper::convertToDTO).toList();
+        return ResponseEntity.ok(list);
     }
 }
