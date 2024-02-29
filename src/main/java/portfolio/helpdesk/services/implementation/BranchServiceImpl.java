c3ac592a71c98e86c92c40a742952e68e1785f64
Daniel Gonzales <danielsmart5@hotmail.com>
2024-02-29 18:08:07 -0500

    Eliminación de métodos sin usar

--- src/main/java/portfolio/helpdesk/services/implementation/BranchServiceImpl.java
+++ src/main/java/portfolio/helpdesk/services/implementation/BranchServiceImpl.java
@@ -3,12 +3,9 @@ package portfolio.helpdesk.services.implementation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import portfolio.helpdesk.exceptions.ModelAlreadyExistsException;
-import portfolio.helpdesk.exceptions.ModelNotFoundException;
 import portfolio.helpdesk.models.Branch;
 import portfolio.helpdesk.repositories.IBranchRepo;
 import portfolio.helpdesk.services.IBranchService;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -26,23 +23,6 @@ public class BranchServiceImpl extends CrudImpl<Branch, Integer> implements IBra
         getRepo().findBranchByNameAndCompany_IdCompany(name, idCompany).ifPresent(branch -> {
             throw new ModelAlreadyExistsException("Branch with name " + name + " already exists in company with id " + idCompany);
         });
-    }
-
-    @Override
-    public void updateBranchStatusByCompanyStatus(Integer idCompany, boolean status) {
-        getRepo().findAllBranchByIdCompany(idCompany).forEach(
-                branch -> getRepo().updateBranchStatusByIdBranch(branch.getIdBranch(), status));
-    }
-
-    @Override
-    public void updateBranchStatusByIdBranch(Integer idBranch, boolean status) {
-        getRepo().findById(idBranch).orElseThrow(() -> new ModelNotFoundException(idBranch));
-        getRepo().updateBranchStatusByIdBranch(idBranch, status);
-    }
-
-    @Override
-    public List<Branch> findAllBranchByIdCompany(Integer idCompany) {
-        return getRepo().findAllBranchByIdCompany(idCompany);
     }
 
 }
